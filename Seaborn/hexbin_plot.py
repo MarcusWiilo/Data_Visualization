211fc881f8277fa215c215b7ba8d1b43688087ea
YOUR NAME <YOUR EMAIL ADDRESS>
2016-12-20 16:29:54 -0200

    Testes Seaborn

---
+++ Seaborn/hexbin_plot.py
@@ -0,0 +1,21 @@
+
+# coding: utf-8
+
+# In[2]:
+
+get_ipython().magic(u'matplotlib inline')
+
+
+# In[3]:
+
+import numpy as np
+from scipy.stats import kendalltau
+import seaborn as sns
+sns.set(style="ticks")
+
+rs = np.random.RandomState(11)
+x = rs.gamma(2, size=1000)
+y = -.5 * x + rs.normal(size=1000)
+
+sns.jointplot(x, y, kind="hex", stat_func=kendalltau, color="#4CB391")
+
